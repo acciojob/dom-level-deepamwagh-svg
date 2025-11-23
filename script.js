a75752f8de44b0f152b9176b8b4499eb65c7bbbf
Deepam Wagh <deepam.wagh@gmail.com>
2025-11-23 21:30:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const elem = document.getElementById("level");
+
+let level = 1;
+
+let parent = elem.parentElement;
+
+while (parent) {
+    level++;
+    parent = parent.parentElement;
+}
+
+alert("The level of the element is: " + level);
